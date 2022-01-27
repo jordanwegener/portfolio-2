b937d505a701f0535cd28383d0eaa8edc2746022
Jordan <jordanpwegener@gmail.com>
2022-01-28 10:54:02 +1100

    continued working on navbar

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React from "react";
+
+//Components 
+
+import Nav from './Nav'
 
 // MUI Imports
 
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button"
-import useTheme from '@mui/material/styles'
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import useTheme from "@mui/material/styles";
 
 const Intro = () => {
+  return (
+    <Box>
+    <Nav/>
+      <Typography variant="h2" textAlign="center">
+        Hello, my name is <Typography variant="h1">
+          Jordan Wegener
+        </Typography>{" "}
+        and I'm a <Typography variant="h1">Software Engineer</Typography>
+      </Typography>
 
-    return(
-        <Box width="50%" justifyContent="center">
-        <Typography variant="h2">
-          Hello, my name is <Typography variant="h1">
-            Jordan Wegener
-          </Typography>{" "}
-          and I'm a <Typography variant="h1">Software Engineer</Typography>
-        </Typography>
-        <Button variant='primary'>Say hello</Button>
-      </Box>
-    )
-}
+      <Button variant="primary">Say hello</Button>
+    </Box>
+  );
+};
 
 export default Intro;
